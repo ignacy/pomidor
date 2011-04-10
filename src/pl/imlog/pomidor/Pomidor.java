eb9baa0afe7bccc711c3781594712deab80b5af1
Ignacy Moryc <imoryc@gmail.com>
2011-04-10 18:13:53 +0200

    Properites setting finally works as expected

--- src/pl/imlog/pomidor/Pomidor.java
+++ src/pl/imlog/pomidor/Pomidor.java
@@ -6,14 +6,17 @@ import android.content.Intent;
 import android.view.View;
 import android.widget.TextView;
 import android.view.View.OnClickListener;
+import android.content.Context;
 import android.os.Handler;
 import android.os.SystemClock;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
+import android.util.Log;
 
 public class Pomidor extends Activity implements OnClickListener
 {
     private static int donePomodoros = 0;
+    private static final int INTERVAL = 62;
 
     private Handler handler;
     private TextView textView;
@@ -21,33 +24,44 @@ public class Pomidor extends Activity implements OnClickListener
     private int mStartTime;
 
     @Override
-        public void onCreate(Bundle savedInstanceState)
+    public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        handler = new Handler();
-        //mStartTime = 1500;
-        mStartTime = 63;
+        try {
+            handler = new Handler();
+            //mStartTime = 1500;
+            mStartTime = INTERVAL;
 
-        View startButton = findViewById(R.id.start_button);
-        startButton.setOnClickListener(this);
-        View stopButton = findViewById(R.id.stop_button);
-        stopButton.setOnClickListener(this);
+            View startButton = findViewById(R.id.start_button);
+            startButton.setOnClickListener(this);
+            View stopButton = findViewById(R.id.stop_button);
+            stopButton.setOnClickListener(this);
 
-        textView = (TextView) findViewById(R.id.timer_label);
-        pomodoros = (TextView) findViewById(R.id.pomodoros_count);
+            textView = (TextView) findViewById(R.id.timer_label);
+            pomodoros = (TextView) findViewById(R.id.pomodoros_count);
+
+        } catch (Exception ex) {
+            Log.e("onCreate", ex.getMessage());
+        }
+
     }
 
 
     public void onClick(View v) {
-        switch (v.getId()) {
-        case R.id.start_button:
-            handler.postDelayed(mUpdateTimeTask, 1000);
-            break;
-        case R.id.stop_button:
-            handler.removeCallbacks(mUpdateTimeTask);
-            break;
+
+        try {
+            switch (v.getId()) {
+            case R.id.start_button:
+                handler.postDelayed(mUpdateTimeTask, 1000);
+                break;
+            case R.id.stop_button:
+                handler.removeCallbacks(mUpdateTimeTask);
+                break;
+            }
+        } catch (Exception ex) {
+            Log.e("onClick", ex.getMessage());
         }
     }
 
@@ -63,19 +77,21 @@ public class Pomidor extends Activity implements OnClickListener
                 if (mStartTime >= 1) {
                     handler.postDelayed(mUpdateTimeTask, 1000);
                 } else {
-                    textView.setText("25:00");
+                    textView.setText(Integer.toString(INTERVAL));
                     donePomodoros++;
 
                     // Save the current value
                     getPreferences(MODE_PRIVATE).edit().putInt("pomodorod_done", donePomodoros).commit();
-
-                    pomodoros.setText(Integer.toString(Pomidor.getDonePomodoros(getContext())));
+                    mStartTime = INTERVAL;
+                    pomodoros.setText(Integer.toString(getDonePomodorosCount()));
                 }
             }
         };
 
-    public static int getDonePomodorosCount(Context context) {
-        return PreferenceManager.getDefaultSharedPreferences(context)
+    public int getDonePomodorosCount() {
+        int curr = PreferenceManager.getDefaultSharedPreferences(this)
             .getInt("pomodorosDone", donePomodoros);
+        Log.i("ccc", "Current count is : " + curr);
+        return curr;
     }
 }
